107283c3205686a76c5429c5fa8d96ceac315040
Kai Mayer <kai.mayer@t-online.de>
2022-06-27 17:08:07 +0200

    header added

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
 <template>
+  <div>
+    <div class="header">
+      <img class="logo" src="@/assets/logo_2500x500_free.png" />
+      <div class="navbar">
+        <router-link to="/">dieKlingel</router-link>
+        <router-link to="/a">the app</router-link>
+        <router-link to="/b">hardware</router-link>
+        <router-link to="/c">software</router-link>
+        <router-link to="/">contact</router-link>
+        <router-link to="/">aboutu us</router-link>
+      </div>
+    </div>
+    <div class="body"></div>
+    <div class="footer"></div>
+  </div>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -18,25 +33,55 @@ import FooterBar from "./components/FooterBar.vue";
 export default class App extends Vue {}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass">
+html, body
+  margin: 0
+  padding: 0
+  font-family: 'Noto Sans JP', sans-serif
 
-nav {
-  padding: 30px;
+body
+  height: 100%
+  display: grid
+  grid-template-rows: auto 1fr auto
+</style>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<style lang="sass" scoped>
+.header
+  background-color: #ccc
+  text-align: center
+  position: relative
+  padding: 10px 0 5vw 0px
+  &:before
+    content: ""
+    left: 0,
+    bottom: 0,
+    width: 100%,
+    position: absolute
+    height: 3.4vw
+    background-color: transparent
+    background-image: linear-gradient(to bottom right, #cccccc 49% , white 51%)
+  .logo
+    max-width: 80%
+    max-height: 120px
+    margin: 20px 0px 5px 0px
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+.navbar
+  background-color: #333
+  margin-top: 5px
+  display: flex
+  flex-wrap: wrap
+  justify-content: space-evenly
+  box-shadow: 2px 2px 5px #808080
+  & a
+    flex: 1 1 auto
+    color: #f2f2f2
+    text-decoration: none
+    box-sizing: border-box
+    padding: 14px 16px
+    &.router-link-exact-active
+      color: #ffc847
+      font-weight: bold
+    &:hover
+      color: #333
+      background-color: #ccc
 </style>
