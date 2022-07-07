e862b71c2056a05ac647240302c2d6a9157df47e
Kai Mayer <kai.mayer@t-online.de>
2022-07-07 15:59:26 +0200

    publicPath for custom domain

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dieklingel-homepage/'
+    ? '/' // custom domainname dieklingel.de
+    // ? '/dieklingel-homepage/' // github pages
     : '/',
   outputDir: 'docs',
   transpileDependencies: true,
