506f3f8c9af56b28768e30becab2fd058fc4b76a
KoiFresh <60545012+KoiFresh@users.noreply.github.com>
2022-07-10 17:47:09 +0000

    Build: (615afa8) build to dist

---
+++ js/323.d01fa8da.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdieklingel_homepage"]=self["webpackChunkdieklingel_homepage"]||[]).push([[323],{6323:function(e,n,t){t.r(n),t.d(n,{default:function(){return f}});var a=t(3396);const c=e=>((0,a.dD)("data-v-5be89fde"),e=e(),(0,a.Cn)(),e),d={class:"content"},o=c((()=>(0,a._)("h1",null,"comming soon...",-1))),i=[o];function r(e,n){return(0,a.wg)(),(0,a.iD)("div",d,i)}var s=t(89);const u={},l=(0,s.Z)(u,[["render",r],["__scopeId","data-v-5be89fde"]]);var f=l}}]);
+//# sourceMappingURL=323.d01fa8da.js.map
